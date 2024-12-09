2de7dc2f195fac5b60fca6e7c1cf4aa9c1084292
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-12-09 12:57:51 +0100

    Init created

---
+++ controller.js
@@ -0,0 +1,11 @@
+import * as model from './model.js';
+import * as view from './view.js';
+
+window.addEventListener('load', init);
+
+function init(){
+    console.log("controller.js loaded");
+
+    model.init();
+    view.init();
+}
