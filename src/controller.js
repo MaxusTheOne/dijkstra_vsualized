419ce5f614995a4932cd8b2454b20fe036d65a71
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-12-09 15:20:27 +0100

    Attempt at making a random tree

--- src/controller.js
+++ src/controller.js
@@ -8,4 +8,30 @@ function init() {
 
   model.init();
   view.init();
+
+  setInterval(addRandomNode, 500);
+}
+
+function addRandomNode() {
+  const node = {
+    x: Math.random(),
+    y: Math.random(),
+    size: Math.random() * 10 + 10,
+    color: '#' + Math.floor(Math.random() * 16777215).toString(16),
+  };
+  const NodeId = Math.floor(Math.random() * view.nodeInstances) + 1;
+  const targetNodeId = nextViableNode(NodeId);
+  view.addNodeWithConnection(node, targetNodeId);
+}
+
+function nextViableNode(nodeId) {
+  const connections = view.graph.degreeWithoutSelfLoops(nodeId);
+
+  if (connections > 2) {
+    nextViableNode(nodeId + 1);
+    return;
+  }
+  console.log('added note with connections: ', connections);
+
+  return nodeId;
 }
--- src/view.js
+++ src/view.js
@@ -1,7 +1,13 @@
 import Graph from 'graphology';
+import chroma from 'chroma-js';
+import ForceSupervisor from 'graphology-layout-force/worker';
 import Sigma from 'sigma';
 
-let graphInstance;
+export let graph;
+let draggedNode = null;
+let isDragging = false;
+let renderer;
+export let nodeInstances = 1;
 
 export function init() {
   console.log('view.js loaded');
@@ -10,11 +16,75 @@ export function init() {
 }
 
 export function initGraph() {
-  const graph = new Graph();
-  graph.addNode('1', { label: 'Node 1', x: 0, y: 0, size: 10, color: 'blue' });
-  graph.addNode('2', { label: 'Node 2', x: 1, y: 1, size: 20, color: 'red' });
-  graph.addEdge('1', '2', { size: 5, color: 'purple' });
+  graph = new Graph();
+  graph.addNode('1', { x: 0, y: 0, size: 10, color: chroma.random().hex() });
 
-  const sigmaInstance = new Sigma(graph, document.getElementById('screen'));
-  console.log('sigmaInstance', sigmaInstance);
+  // Create the spring layout and start it
+  const layout = new ForceSupervisor(graph, {
+    isNodeFixed: (_, attr) => {
+      attr.highlighted;
+    },
+    settings: {
+      gravity: 0.0000002,
+      inertia: 0.4,
+      repulsion: 0.4,
+      attraction: 0.0003,
+    },
+  });
+  layout.start();
+
+  // Create the sigma
+  renderer = new Sigma(graph, document.getElementById('screen'));
+
+  // State for drag'n'drop
+
+  // On mouse down on a node
+  //  - we enable the drag mode
+  //  - save in the dragged node in the state
+  //  - highlight the node
+  //  - disable the camera so its state is not updated
+  renderer.on('downNode', (e) => {
+    isDragging = true;
+    draggedNode = e.node;
+    graph.setNodeAttribute(draggedNode, 'highlighted', true);
+    if (!renderer.getCustomBBox()) renderer.setCustomBBox(renderer.getBBox());
+  });
+
+  // On mouse move, if the drag mode is enabled, we change the position of the draggedNode
+  renderer.on('moveBody', ({ event }) => {
+    if (!isDragging || !draggedNode) return;
+
+    // Get new position of node
+    const pos = renderer.viewportToGraph(event);
+
+    graph.setNodeAttribute(draggedNode, 'x', pos.x);
+    graph.setNodeAttribute(draggedNode, 'y', pos.y);
+
+    // Prevent sigma to move camera:
+    event.preventSigmaDefault();
+    event.original.preventDefault();
+    event.original.stopPropagation();
+  });
+
+  // On mouse up, we reset the dragging mode
+  const handleUp = () => {
+    if (draggedNode) {
+      graph.removeNodeAttribute(draggedNode, 'highlighted');
+    }
+    isDragging = false;
+    draggedNode = null;
+  };
+  renderer.on('upNode', handleUp);
+  renderer.on('upStage', handleUp);
+}
+
+export function addNodeWithConnection(node, targetNodeId) {
+  graph.addNode(++nodeInstances, {
+    ...node,
+    label: `Node ${nodeInstances}`,
+  });
+  graph.addEdge(nodeInstances, targetNodeId, {
+    size: 5,
+    color: 'purple',
+  });
 }
