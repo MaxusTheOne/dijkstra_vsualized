89eb21530178774f5664ceecba78d523c71bac96
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-12-22 15:55:20 +0100

    Leaflet rework

--- src/view.js
+++ src/view.js
@@ -1,55 +1,127 @@
+import 'leaflet/dist/leaflet.css';
+
 import chroma from 'chroma-js';
 import Graph from 'graphology';
-import Sigma from 'sigma';
+import L from 'leaflet';
 
 // import { Settings } from "sigma/src/settings";
 
 export let graph;
-let draggedNode = null;
-let isDragging = false;
-let renderer, mouseCaptor, viewportCoords;
+let map;
 export let nodeInstances = 1;
+let selectedNodes = [];
+let isHoveringNode = false;
 
 export async function init() {
   console.log('view.js loaded');
 
+  initMap();
   initGraph();
   await loadJson();
 }
 
-export function initGraph() {
-  graph = new Graph();
-  // Create the sigma
-  renderer = new Sigma(graph, document.getElementById('sigma_container'), {
-    settings: {
-      height: 800,
-      width: 800,
-      enableCameraZooming: false,
-      enableCameraPan: false,
-    },
-  });
+export function initMap() {
+  map = L.map('map_container').setView([55, 10], 4);
 
-  const canvas = document.getElementById('screen');
-  canvas.addEventListener('mousedown', (e) => {
-    const viewportCoords = renderer.viewportToGraph({
-      x: e.clientX,
-      y: e.clientY,
-    });
-    console.log('mousedown at:', viewportCoords.x, viewportCoords.y);
-    // controller.addNode(viewportCoords.x, viewportCoords.y);
-  });
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
-  console.log('graph', renderer);
+  map.on('click', onMapClick);
+
+  console.log('map initialized', map);
 }
 
-export function addNode(x, y) {
-  graph.addNode(++nodeInstances, {
-    x,
-    y,
-    size: 10,
+function onMapClick(e) {
+  const { lat, lng } = e.latlng;
+  const radius = 30000; // Radius of the nodes in meters[55, 10], 4
+  let nearestNode;
+
+  let isNearNode = false;
+  graph.forEachNode((nodeId, attributes) => {
+    const distance = map.distance([lat, lng], [attributes.lat, attributes.lng]);
+    if (distance < radius) {
+      isNearNode = true;
+    }
+    if (nearestNode === undefined || distance < nearestNode.distance) {
+      nearestNode = { nodeId, distance };
+    }
+  });
+  console.log('nearestNode', nearestNode);
+  if (!isNearNode) {
+    console.log('click at:', lat, lng);
+    addNode(lat, lng);
+  } else {
+    console.log('');
+  }
+}
+
+export function initGraph() {
+  graph = new Graph();
+}
+
+export function addNode(lat, lng, name) {
+  const nodeId = ++nodeInstances;
+  let node = graph.addNode(nodeId, {
+    lat,
+    lng,
+    size: 100,
     color: chroma.random().hex(),
   });
-  console.log('added node', nodeInstances);
+
+  const circle = L.circle([lat, lng], {
+    color: 'red',
+    radius: 30000, // Increase the radius to make the nodes bigger
+  }).addTo(map);
+
+  circle.on('click', (e) => {
+    e.originalEvent.stopPropagation(); // Prevent map click event
+    handleNodeClick(nodeId);
+  });
+  circle.bindTooltip(`${name}`, {
+    permanent: true,
+    direction: 'center',
+    className: 'polyline-label',
+    direction: 'top',
+  });
+}
+
+function handleNodeClick(nodeId) {
+  selectedNodes.push(nodeId);
+  if (selectedNodes.length === 2) {
+    const [node1, node2] = selectedNodes;
+    addEdge(node1, node2);
+    selectedNodes = [];
+  }
+}
+
+function addEdge(node1Id, node2Id) {
+  const node1 = graph.getNodeAttributes(node1Id);
+  const node2 = graph.getNodeAttributes(node2Id);
+
+  graph.addEdge(node1Id, node2Id, {
+    size: 5,
+    color: 'purple',
+  });
+
+  const polyline = L.polyline(
+    [
+      [node1.lat, node1.lng],
+      [node2.lat, node2.lng],
+    ],
+    {
+      color: 'purple',
+      weight: 5,
+    }
+  ).addTo(map);
+
+  polyline.bindTooltip(`∞`, {
+    permanent: true,
+    direction: 'center',
+    className: 'polyline-label',
+  });
+  console.log(`added edge between node ${node1Id} and node ${node2Id}`);
 }
 
 export function addNodeWithConnection(node, targetNodeId) {
@@ -57,10 +129,18 @@ export function addNodeWithConnection(node, targetNodeId) {
     ...node,
     label: `Node ${nodeInstances}`,
   });
-  graph.addEdge(nodeInstances, targetNodeId, {
-    size: 5,
-    color: 'purple',
-  });
+
+  const targetNode = graph.getNodeAttributes(targetNodeId);
+  L.polyline(
+    [
+      [node.lat, node.lng],
+      [targetNode.lat, targetNode.lng],
+    ],
+    {
+      color: 'purple',
+      weight: 5,
+    }
+  ).addTo(map);
 }
 
 async function loadJson() {
@@ -69,7 +149,10 @@ async function loadJson() {
     .then((data) => {
       console.log(data);
       data.nodes.forEach((node) => {
-        addNode(node.x, node.y);
+        addNode(node.lat, node.lng, node.name);
+      });
+      data.edges.forEach((edge) => {
+        addEdge(edge.source, edge.target);
       });
     });
 }
