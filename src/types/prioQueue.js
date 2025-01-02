3d01a9b4ea96a60564a8147d891fba719f260671
TheDanishMexican <danieljchristjansen@gmail.com>
2025-01-02 13:44:53 +0100

    added comments to prioQueue.js file

--- src/types/prioQueue.js
+++ src/types/prioQueue.js
@@ -3,15 +3,22 @@ export default class PrioQueue {
     this.queue = [];
   }
 
+  //priority will always be the distance of the node from the start node to the node itself
+
+  //sort is used to sort all items in the queue after priority, which is this said distance
   enqueue(node, priority) {
     this.queue.push({ node, priority });
     this.sort();
   }
 
+  //this uses the shift method of js arrays, which always returns the first element, and 
+  //since we have sort the array on each enqueue, the first item will always be the one
+  //with the shortest distance to the start node
   dequeue() {
     return this.queue.shift();
   }
 
+  //NOT IN USE, DELETE THIS
   updatePriority(node, newPriority) {
     this.queue = this.queue.map((item) => {
       if (item.node === node) {
@@ -22,14 +29,18 @@ export default class PrioQueue {
     this.sort();
   }
 
+  //shows the data inside the queue at this moment in time
   dump() {
     console.log(this.queue);
   }
 
+  //returns true or false if the queue is empty or not
   isEmpty() {
     return this.queue.length === 0;
   }
 
+  //this is the sort method which is used each time we enqueue, the priority is the distance
+  //of the node from the start node
   sort() {
     this.queue.sort((a, b) => a.priority - b.priority);
   }
